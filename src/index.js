befbaf35f57f80afaec28c7ea4fd25a4dcb6c59d
Артемчук Алексей <elusiv@mail.ru>
2022-07-14 00:18:20 +0500

    Completed generator 13.07.22

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './css/style.css';
 
-import './js/GameSavingLoader';
+import './js/Team';
--- src/js/GameSaving.js
+++
@@ -1,8 +0,0 @@
-export default class GameSaving {
-  constructor(saving) {
-    const saveHero = JSON.parse(saving);
-    this.id = saveHero.id;
-    this.created = saveHero.created;
-    this.userInfo = saveHero.userInfo;
-  }
-}
